666b99c28930853623305ce0467628d56620b153
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-01-23 19:06:23 +0100

    Added hyperfine.

--- run.bat
+++ run.bat
@@ -1,20 +1,43 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if "%~1"=="1" (
-    cd csharp\version1
-    call build.bat
-    call run.bat
+set working_dir=%cd%
+
+echo.
+
+if "%~1"=="0" (
+    call :all
+    exit /b 0
+) else if "%~1"=="1" (
+    call :csharp_version1
     exit /b 0
 ) else if "%~1"=="2" (
-    cd csharp\version1
-    call build.bat aot
-    call run.bat aot
+    call :csharp_version1_aot
     exit /b 0
 ) else (
     call :usage
     exit /b 1
 )
+
+:csharp_version1
+    cd  %working_dir%
+    cd csharp\version1
+    call build.bat > nul
+	hyperfine -i --output=pipe --runs 3 --warmup 2 -n "C#" "run.bat"
+    exit /b 0
+
+:csharp_version1_aot
+    cd  %working_dir%
+    cd csharp\version1
+    call build.bat aot > nul
+	hyperfine -i --output=pipe --runs 3 --warmup 2 -n "C# AOT" "run.bat aot"
+    exit /b 0
+
+:all
+    cd  %working_dir%
+    call :csharp_version1
+    call :csharp_version1_aot
+    exit /b 0
 
 :usage
 echo.
@@ -22,6 +45,7 @@ echo Invalid argument.
 echo Usage: %~nx0 ^<implementation number^>
 echo.
 echo Implementations:
+echo 0: All
 echo 1: C#
 echo 2: C# AOT
 echo.
--- run.sh
+++ run.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
+working_dir="$(pwd)"
+
+echo ""
+
 usage() {
   echo ""
   echo "Invalid argument!"
   echo "Usage: $0 <implementation number>"
   echo ""
   echo "Implementations:"
+  echo "0: All"
   echo "1: C#"
   echo "2: C# AOT"
   exit 1
 }
 
-if [ "$1" = "1" ]; then
+csharp_version1() {
+  cd "$working_dir"
   cd csharp/version1
-  bash build.sh
-  bash run.sh
+  bash build.sh >/dev/null
+  hyperfine -i --output=pipe --runs 3 --warmup 2 -n "C#" "bash run.sh"
+}
+
+csharp_version1_aot() {
+  cd "$working_dir"
+  cd csharp/version1
+  bash build.sh aot >/dev/null
+  hyperfine -i --output=pipe --runs 3 --warmup 2 -n "C# AOT" "bash run.sh aot"
+}
+
+all() {
+  cd "$working_dir"
+  csharp_version1
+  csharp_version1_aot
+}
+
+if [ "$1" = "0" ]; then
+  all
+elif [ "$1" = "1" ]; then
+  csharp_version1
 elif [ "$1" = "2" ]; then
-  cd csharp/version1
-  bash build.sh aot
-  bash run.sh aot
+  csharp_version1_aot
 else
   usage
 fi
